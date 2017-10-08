1393532720465b3c31a2aa6918ab4f7faf7a39de
adityakalushte <aditya_kalushte@rediffmail.com>
2017-10-08 16:13:49 +0530

    adding config and ionic commands

---
+++ firebase_config_settings.js
@@ -0,0 +1,20 @@
+// Original Toastmasters Project: https://bitbucket.org/mnvsngv/toastmasters-app
+// Github Toastmasters Project: https://github.com/adityakalushte/technophilia-2017-app
+// Project Console: https://console.firebase.google.com/project/technophilia-2017-app/overview
+// Hosting URL: https://technophilia-2017-app.firebaseapp.com
+
+<script src="https://www.gstatic.com/firebasejs/4.5.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD30BbTsfissawqmEgN6sqZnJ66fcArt78",
+    authDomain: "technophilia-2017-app.firebaseapp.com",
+    databaseURL: "https://technophilia-2017-app.firebaseio.com",
+    projectId: "technophilia-2017-app",
+    storageBucket: "technophilia-2017-app.appspot.com",
+    messagingSenderId: "521949716423"
+  };
+  firebase.initializeApp(config);
+</script>
+
+
